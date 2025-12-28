40b2c1bce0fd7f2cae6e25a4002fd8008a54bbea
Daemme <luiz.daemme@gmail.com>
2025-12-28 16:33:30 -0300

    Add circleshape.py and player.py to implemente the spaceship movements

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ import pygame
 from logger import log_state
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     delta = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+        dt = clock.tick(60) / 1000
         for event in pygame.event.get():
+            player.update(dt)
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
-    print(f"Starting Asteroids with pygame version: VERSION")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        
 
 if __name__ == "__main__":
     main()
