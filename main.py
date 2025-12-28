faa5685490657709b4e151ac88cc3f0b8f7e558a
Daemme <luiz.daemme@gmail.com>
2025-12-28 11:58:59 -0300

    Initial config, draw the screen and set the FPS

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+from logger import log_state
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    delta = 0
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color="black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    print(f"Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
